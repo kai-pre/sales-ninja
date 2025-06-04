554eb8568779ae40cfb4152133537ed39efafa49
Kai Preuß <kai.preuss@protonmail.com>
2025-06-04 09:28:46 +0200

    Wednesday morning checkpoint for merge

--- salesninja/data.py
+++ salesninja/data.py
@@ -1,5 +1,5 @@
 ### Imports
-
+import pandas as pd
 
 
 
@@ -7,4 +7,17 @@
 
 class SalesNinja():
     def get_data(self):
-        pass
+        data = pd.read_csv("../raw_data/FactSales.csv")
+        data.set_index(['SalesKey'], inplace=True)
+
+        ### TO DO:
+        ### join appropriate extra tables:
+        ### DimStore
+        ### DimGeography (only reachable by DimStore)
+        ### DimProduct
+        ### DimProductSubcategory (only reachable by DimProduct)
+        ### DimProductCategory (only reachable by DimProductSubcategory)
+        ### DimPromotion
+        #data = pd.merge(data, pd.read_csv("../raw_data/DimStore.csv"), on = "StoreKey")
+
+        return data
