96a06d7dd75728050ee9f737be2113abb32a15d1
Kai Preuß <kai.preuss@protonmail.com>
2025-06-05 14:42:35 +0200

    extended file structure

---
+++ salesninja/interface/main.py
@@ -0,0 +1,17 @@
+### Imports
+
+
+
+
+### Main interface for SalesNinja dashboard
+def load_and_preprocess():
+    pass
+
+def train():
+    pass
+
+def evaluate():
+    pass
+
+def predict():
+    pass
